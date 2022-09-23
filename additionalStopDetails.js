6b4e2a0e805c2a6e4c4c7400bc8e436fca17f85e
Julian Labeit <j.labeit@impargo.de>
2022-09-23 16:39:31 +0200

    Update API example to use new importOrder API

---
+++ additionalStopDetails.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+const fetch = require("node-fetch")
+const data = require("./data/advancedOrder.json")
+const endpoint = process.env.ENDPOINT || "https://dev.backend.impargo.eu/"
+const TOKEN = process.env.TOKEN
+const query = `
+mutation importOrder($data: OrderImportInput!){
+  importOrder(data:$data) {
+    _id
+    order {
+      reference
+      route {
+        distance
+        time
+      }
+    }
+  }
+}
+`
+
+fetch(endpoint, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "authorization": TOKEN
+  },
+  body: JSON.stringify({
+    query: query,
+    variables: {
+      data,
+    },
+  })
+})
+  .then(result => {
+    return result.json();
+  })
+  .then(result => {
+    console.log(result)
+    console.log("Order successfully created:\n", JSON.stringify(result.data, null, 2));
+  });
--- simple.js
+++ simple.js
@@ -1,15 +1,18 @@
 #!/usr/bin/env node
 const fetch = require("node-fetch")
-const data = require("./orderData.json")
-const endpoint = "https://dev.backend.impargo.eu/"
+const data = require("./data/simpleOrder.json")
+const endpoint = process.env.ENDPOINT || "https://dev.backend.impargo.eu/"
 const TOKEN = process.env.TOKEN
 const query = `
-mutation createCompanyOrder($data: CompanyOrderInput!) {
-  createCompanyOrder(data:$data) {
+mutation importOrder($data: OrderImportInput!){
+  importOrder(data:$data) {
     _id
     order {
-      _id
       reference
+      route {
+        distance
+        time
+      }
     }
   }
 }
@@ -31,6 +34,6 @@ fetch(endpoint, {
   .then(result => {
     return result.json();
   })
-  .then(data => {
-    console.log("Order successfully created:\n", data);
+  .then(result => {
+    console.log("Order successfully created:\n", JSON.stringify(result.data, null, 2));
   });
